25a7e6ad311df6ffef342153418045b3406182e8
QQ <q.lenhuquynh@gmail.com>
2018-07-15 13:34:14 +0300

    format prettier

--- src/containers/new-post.js
+++ src/containers/new-post.js
@@ -1,80 +1,70 @@
-import React from "react";
-import { addPost, generateDate } from "../actions";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { Field, reduxForm } from "redux-form";
+import React from 'react'
+import { addPost, generateDate } from '../actions'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Field, reduxForm } from 'redux-form'
 
 class PostNew extends React.Component {
   onSubmit = values => {
-    const { keys, login_u, generateDate, addPost } = this.props;
-    const copy_values = { ...values };
-    copy_values.id = Number(keys[keys.length - 1]) + 1;
-    copy_values.author = login_u.username;
-    copy_values.date = generateDate().payload;
+    const { keys, login_u, generateDate, addPost } = this.props
+    const copy_values = { ...values }
+    copy_values.id = Number(keys[keys.length - 1]) + 1
+    copy_values.author = login_u.username
+    copy_values.date = generateDate().payload
     addPost(copy_values, () => {
-      this.props.history.push("/");
-    });
-  };
+      this.props.history.push('/')
+    })
+  }
 
   renderInput = props => {
-    console.log("props", props);
+    console.log('props', props)
     const {
       meta: { touched, error }
-    } = props;
+    } = props
     return (
       <div>
         <label>{props.label}</label>
         <input type="text" {...props.input} />
-        <p className="err">{touched ? error : ""}</p>
+        <p className="err">{touched ? error : ''}</p>
       </div>
-    );
-  };
+    )
+  }
 
   renderSelect = ({ meta, label, input, children }) => {
-    const { touched, error } = meta;
+    const { touched, error } = meta
     return (
       <div>
         <label>{label}</label>
         <select {...input}>{children}</select>
-        <p className="err">{touched ? error : ""}</p>
+        <p className="err">{touched ? error : ''}</p>
       </div>
-    );
-  };
+    )
+  }
 
   renderTextarea = ({ meta, label, input }) => {
-    const { touched, error } = meta;
+    const { touched, error } = meta
     // const {meta: {touched, error}} = props
     return (
       <div>
         <label>{label}</label>
         <textarea type="text" {...input} />
-        <p className="err">{touched ? error : ""}</p>
+        <p className="err">{touched ? error : ''}</p>
       </div>
-    );
-  };
+    )
+  }
 
   render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit } = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <h4>Create new post</h4>
-        <Field
-          label="Title: "
-          name="title"
-          component={this.renderInput}
-          type="text"
-        />
+        <Field label="Title: " name="title" component={this.renderInput} type="text" />
         <Field label="Category: " name="category" component={this.renderSelect}>
           <option value="culture">Culture</option>
           <option value="lifestyle">Lifestyle</option>
           <option value="tech">Tech</option>
         </Field>
-        <Field
-          label="Content: "
-          name="content"
-          component={this.renderTextarea}
-          type="text"
-        />
+        <Field label="Content: " name="content" component={this.renderTextarea} type="text" />
         <div id="save-cancel">
           <button type="submit">Save</button>
           <button>
@@ -82,31 +72,31 @@ class PostNew extends React.Component {
           </button>
         </div>
       </form>
-    );
+    )
   }
 }
 
 const mapStateToProps = ({ posts, login, date }) => ({
   keys: Object.keys(posts),
   login_u: login
-});
+})
 
 const validate = values => {
   //name must be same as keys of errors
   const errors = {
-    title: !values.title ? "Enter a title" : "",
-    category: !values.category ? "Enter a category" : "",
-    content: !values.content ? "Enter some content" : ""
-  };
-  return errors;
-};
+    title: !values.title ? 'Enter a title' : '',
+    category: !values.category ? 'Enter a category' : '',
+    content: !values.content ? 'Enter some content' : ''
+  }
+  return errors
+}
 
 export default reduxForm({
   validate,
-  form: "NewPost"
+  form: 'NewPost'
 })(
   connect(
     mapStateToProps,
     { addPost, generateDate }
   )(PostNew)
-);
+)
